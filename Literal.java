8dd6c75e5a62b31cbf37bee6f45ed2576f66e84b
TASNIA <164462424+Tasnia22@users.noreply.github.com>
2024-04-01 23:12:35 +0600

    Add files via upload

---
+++ Literal.java
@@ -0,0 +1,27 @@
+public class Literal
+{
+    public static void main(String[]args)
+    {
+        int num=12;
+        float min=189.8f;
+        double r=1234.67;
+        int hexa=0x14;
+        int binary=0b11101;
+        int octval=024;
+        char T = 'o'; 
+        boolean p=true;
+        String str="Tasnia Rahman";
+
+        System.out.println(num);
+        System.out.println(min);
+        System.out.println(r);
+        System.out.println(hexa);
+        System.out.println(binary);
+        System.out.println(octval);
+        System.out.println(T);
+        System.out.println(p);
+        System.out.println(str);
+
+
+    }
+}
