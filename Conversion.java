9b35c21316eea0c2eb5080acc69a2622cdb19a16
TASNIA <164462424+Tasnia22@users.noreply.github.com>
2024-04-02 06:49:57 +0600

    Add files via upload

---
+++ Conversion.java
@@ -0,0 +1,9 @@
+public class Conversion {
+    public static void main(String[] args) {
+        int num1=78;
+        float num2;
+        num2=num1;
+        System.out.println(num2);
+    }
+    
+}
