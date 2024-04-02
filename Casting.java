7f0e5e734aea91e7047b1feb388483978b976ff9
TASNIA <164462424+Tasnia22@users.noreply.github.com>
2024-04-02 06:43:23 +0600

    Add files via upload

---
+++ Casting.java
@@ -0,0 +1,11 @@
+public class Casting {
+    public static void main(String[]args)
+    {
+        double D=1234.90;
+        int P=(int)D;
+        System.out.println(D);
+        System.out.println(P);
+
+    }
+    
+}
