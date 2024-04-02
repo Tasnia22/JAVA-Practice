784bad322d7de5170a854dfe2a965ab831899fdd
TASNIA <164462424+Tasnia22@users.noreply.github.com>
2024-04-02 16:07:05 +0600

    Add files via upload

---
+++ Conditional.java
@@ -0,0 +1,26 @@
+public class Conditional {
+    public static void main(String[]args)
+    {
+        int x= 56;
+        int f= 54;
+        int G= 44;
+
+        
+         if(x>f && f>G)
+         {
+            System.out.println(x);
+         }
+         else
+             System.out.println(G);    
+
+        if(G>f)
+        {
+            System.out.println(f);
+        } 
+        else
+            System.out.println(x);    
+          
+             
+    }
+    
+}
