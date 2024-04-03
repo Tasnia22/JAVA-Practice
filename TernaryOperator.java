42cbd41b32b05415e23f41bc4cb191e7f9e5820e
TASNIA <164462424+Tasnia22@users.noreply.github.com>
2024-04-03 12:58:08 +0600

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,17 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        
+      int n=20,max;
+    int n1 = 43;
+
+    System.out.println("First Num: " +n);
+    System.out.println("Second Num: " +n1);
+
+    max=(n>n1)? n:n1;
+
+    System.out.println("Maximum Number is: "+max);
+      
+
+    }
+    
+}
