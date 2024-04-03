caa697ba1855b3344b3cdeec237682b2e8bf6ffb
TASNIA <164462424+Tasnia22@users.noreply.github.com>
2024-04-03 14:09:40 +0600

    Add files via upload

---
+++ TernaryPractice.java
@@ -0,0 +1,12 @@
+public class TernaryPractice {
+    public static void main(String[]args)
+    {
+        int a=7; 
+        System.out.println("The value is: " +a);
+        
+        
+         String num=(a%2==0)?"The number is even":"The number is odd";
+        System.out.println(num);
+    }
+    
+}
