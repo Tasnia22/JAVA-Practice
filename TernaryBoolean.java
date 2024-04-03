a6d85aa64926e4635af4b8e0f96d6ab0d5b087f6
TASNIA <164462424+Tasnia22@users.noreply.github.com>
2024-04-03 22:32:35 +0600

    Boolean Condition

---
+++ TernaryBoolean.java
@@ -0,0 +1,9 @@
+public class TernaryBoolean {
+    public static void main(String[]args)
+    {
+         boolean fact= false;
+         String result=(fact)?"true":"false";
+         System.out.println(result);
+    }
+    
+}
