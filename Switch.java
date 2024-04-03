c76ecb5aab7e261d2ede85f526af09bf2d559c52
TASNIA <164462424+Tasnia22@users.noreply.github.com>
2024-04-03 15:34:16 +0600

    SwitchCase

---
+++ Switch.java
@@ -0,0 +1,30 @@
+public class Switch {
+    public static void main(String[] args) {
+        int n = 9;
+        switch (n) {
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Skip");
+        }
+    }
+}
