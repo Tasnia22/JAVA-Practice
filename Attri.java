fc043b1313ee904377693cf6689e4722dc9caeb5
TASNIA <164462424+Tasnia22@users.noreply.github.com>
2024-04-05 19:18:40 +0600

    New practice

---
+++ Attri.java
@@ -0,0 +1,15 @@
+public class Attri {
+    int roll=22;
+String name = "TAsnia";
+String section="Sigma";
+
+
+public static void main(String[]args)
+
+{     Attri Doll = new Attri();
+      System.out.println("Name: "+Doll.name);
+      System.out.println("ROll: "+Doll.section);
+      }
+      }
+    
+
