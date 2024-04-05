1f18db20e8d47a3b89edf28e9b5c53afd07026da
TASNIA <164462424+Tasnia22@users.noreply.github.com>
2024-04-05 12:07:44 +0600

    create class and object and accessing attributes.

---
+++ OOP.java
@@ -0,0 +1,23 @@
+public class OOP {
+    // attribute
+
+    int x= 4;
+    public static void main(String[]args)
+    {   
+        
+
+        OOP num1 =new OOP();
+        //here, OOP= class name
+        // num1[variable] = object
+        // new is a keyword
+        // OOP num1 =new OOP(); This is create an object.
+        //The variables of the class is called Object
+
+
+        // accessing attributes
+        System.out.println(num1.x);
+
+
+    }
+    
+}
