f57572bd9a9488af84212a6a3d61773e4d3bce4e
TASNIA <164462424+Tasnia22@users.noreply.github.com>
2024-04-05 19:07:06 +0600

    How to access Multiple attributes

---
+++ MulAttri.java
@@ -0,0 +1,15 @@
+public class MulAttri {
+
+    String name = "Tasnia";
+    String home = "Chandpur";
+    int phone = 12345789;
+    
+
+public static void main(String[]agrs)
+{
+    MulAttri Rahman = new MulAttri();
+    System.out.println(Rahman.name);
+    System.out.println(Rahman.home);
+    System.out.println(Rahman.phone);
+}
+}
