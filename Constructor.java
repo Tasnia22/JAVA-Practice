de5f0c263d0368ce246e08540e666f34aea6feda
TASNIA <164462424+Tasnia22@users.noreply.github.com>
2024-04-08 22:16:43 +0600

    The constructor is called if the object is created
    The constructor is called when the object is created.

---
+++ Constructor.java
@@ -0,0 +1,17 @@
+public class Constructor {
+    int x;  // Create a class attribute
+
+    // Create a class constructor for the Main((COnstructor)class
+    public Constructor()
+    {
+        x = 5;
+
+    }
+    public static void main(String[]agrs)
+    {
+        Constructor num= new Constructor();
+        System.out.println(num.x);
+
+    }
+    
+}
