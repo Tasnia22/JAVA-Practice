fd37eb1930410759e8284af7b795838cb48bc8d6
TASNIA <164462424+Tasnia22@users.noreply.github.com>
2024-04-08 11:22:33 +0600

    Basic array example.

---
+++ Array.java
@@ -0,0 +1,12 @@
+public class Array {
+    public static void main(String[]args)
+    {
+        int num[]={6,6,7,9,0};
+
+        for(int i=0;i<6;i++)
+        {
+            System.out.println(num[i]);
+        }
+    }
+    
+}
